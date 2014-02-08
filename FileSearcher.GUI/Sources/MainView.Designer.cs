c84b46b82b0d091e48cbdd24f0ca255aba36a968
Mike Bardynin <mikebardynin@gmail.com>
2014-02-08 21:12:25 +0400

    редактирование вида главной формы

--- FileSearcher.GUI/Sources/MainView.Designer.cs
+++ FileSearcher.GUI/Sources/MainView.Designer.cs
@@ -46,10 +46,13 @@
 			this.label1 = new System.Windows.Forms.Label();
 			this.txtPluginInfo = new System.Windows.Forms.TextBox();
 			this.btnSelectPlugin = new System.Windows.Forms.Button();
+			this.gbxFilters = new System.Windows.Forms.GroupBox();
+			this.flpFilters = new System.Windows.Forms.FlowLayoutPanel();
 			this.statusStrip.SuspendLayout();
 			this.tableLayoutPanel1.SuspendLayout();
 			this.groupBox1.SuspendLayout();
 			this.flowLayoutPanel1.SuspendLayout();
+			this.gbxFilters.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// statusStrip
@@ -95,7 +98,8 @@
 			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
 			this.tableLayoutPanel1.Controls.Add(this.lvSearchResults, 1, 0);
 			this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
-			this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 2);
+			this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 2);
+			this.tableLayoutPanel1.Controls.Add(this.gbxFilters, 0, 1);
 			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
 			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -168,7 +172,7 @@
 			// 
 			// btnSearch
 			// 
-			this.btnSearch.Location = new System.Drawing.Point(608, 3);
+			this.btnSearch.Location = new System.Drawing.Point(308, 3);
 			this.btnSearch.Name = "btnSearch";
 			this.btnSearch.Size = new System.Drawing.Size(75, 23);
 			this.btnSearch.TabIndex = 3;
@@ -177,20 +181,19 @@
 			// 
 			// flowLayoutPanel1
 			// 
-			this.tableLayoutPanel1.SetColumnSpan(this.flowLayoutPanel1, 2);
 			this.flowLayoutPanel1.Controls.Add(this.btnClear);
 			this.flowLayoutPanel1.Controls.Add(this.btnStop);
 			this.flowLayoutPanel1.Controls.Add(this.btnSearch);
 			this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 560);
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(303, 560);
 			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
 			this.flowLayoutPanel1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-			this.flowLayoutPanel1.Size = new System.Drawing.Size(848, 29);
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(548, 29);
 			this.flowLayoutPanel1.TabIndex = 4;
 			// 
 			// btnClear
 			// 
-			this.btnClear.Location = new System.Drawing.Point(770, 3);
+			this.btnClear.Location = new System.Drawing.Point(470, 3);
 			this.btnClear.Name = "btnClear";
 			this.btnClear.Size = new System.Drawing.Size(75, 23);
 			this.btnClear.TabIndex = 4;
@@ -199,7 +202,7 @@
 			// 
 			// btnStop
 			// 
-			this.btnStop.Location = new System.Drawing.Point(689, 3);
+			this.btnStop.Location = new System.Drawing.Point(389, 3);
 			this.btnStop.Name = "btnStop";
 			this.btnStop.Size = new System.Drawing.Size(75, 23);
 			this.btnStop.TabIndex = 5;
@@ -232,6 +235,28 @@
 			this.btnSelectPlugin.Text = "...";
 			this.btnSelectPlugin.UseVisualStyleBackColor = true;
 			// 
+			// gbxFilters
+			// 
+			this.gbxFilters.AutoSize = true;
+			this.gbxFilters.Controls.Add(this.flpFilters);
+			this.gbxFilters.Dock = System.Windows.Forms.DockStyle.Top;
+			this.gbxFilters.Location = new System.Drawing.Point(3, 110);
+			this.gbxFilters.Name = "gbxFilters";
+			this.tableLayoutPanel1.SetRowSpan(this.gbxFilters, 2);
+			this.gbxFilters.Size = new System.Drawing.Size(294, 19);
+			this.gbxFilters.TabIndex = 5;
+			this.gbxFilters.TabStop = false;
+			this.gbxFilters.Text = "Filters";
+			// 
+			// flpFilters
+			// 
+			this.flpFilters.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.flpFilters.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+			this.flpFilters.Location = new System.Drawing.Point(3, 16);
+			this.flpFilters.Name = "flpFilters";
+			this.flpFilters.Size = new System.Drawing.Size(288, 0);
+			this.flpFilters.TabIndex = 0;
+			// 
 			// MainView
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -245,9 +270,11 @@
 			this.statusStrip.ResumeLayout(false);
 			this.statusStrip.PerformLayout();
 			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
 			this.groupBox1.ResumeLayout(false);
 			this.groupBox1.PerformLayout();
 			this.flowLayoutPanel1.ResumeLayout(false);
+			this.gbxFilters.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -273,6 +300,8 @@
 		private System.Windows.Forms.Button btnSelectPlugin;
 		private System.Windows.Forms.TextBox txtPluginInfo;
 		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.GroupBox gbxFilters;
+		private System.Windows.Forms.FlowLayoutPanel flpFilters;
 
 	}
 }
